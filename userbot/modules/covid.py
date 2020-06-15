2abf0da61ea779855332a4eff67fc2b1547d86fa
HafizZiq <hafizuddinismail552@gmail.com>
2020-06-15 19:37:45 +0800

    covid.py: lets make it mor fensi boi
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -13,19 +13,40 @@ from userbot.events import register
 async def corona(event):
     await event.edit("`Processing...`")
     country = event.pattern_match.group(1)
+    if country:
+        pass
+    elif country.lower() == "south korea" or "korea":
+        country = "s. korea"
+    elif not country:
+        country = "World"
+    else:
+        await event.edit("`Usage: .covid <country>`")
+        return
     covid = Covid(source="worldometers")
     country_data = covid.get_status_by_country_name(country)
     if country_data:
-        output_text =  f"`Confirmed   : {country_data['confirmed']} (+{country_data['new_cases']})`\n"
-        output_text += f"`Active      : {country_data['active']}`\n"
-        output_text += f"`Deaths      : {country_data['deaths']} (+{country_data['new_deaths']})`\n"
-        output_text += f"`Recovered   : {country_data['recovered']}`\n"
-        output_text += f"`Total tests : {country_data['total_tests']}`\n"
+        output_text =  f"`😷Confirmed   : {country_data['confirmed']} (+{country_data['new_cases']})`\n"
+        output_text += f"`🤒Active      : {country_data['active']}`\n"
+        output_text += f"`🤕Critical    : {country_data['critical']}`\n"
+        output_text += f"`⚰Deaths      : {country_data['deaths']} (+{country_data['new_deaths']})`\n"
+        output_text += f"`😇Recovered   : {country_data['recovered']}`\n"
+        if country.lower() == "world":
+            output_text += f"`🧪Total tests : N/A`\n"
+        else:
+            output_text += f"`🧪Total tests : {country_data['total_tests']}`\n"
+        output_text += f"`Data provided by worldometers`"
+        if country:
+            pass
+        elif country = "s. korea":
+            country = "Korea, South"
+        elif country.lower() == "world":
+            output_text += f"`📅Last update : Timer are not yet available for World status`"
+            return
         covid2 = Covid(source="john_hopkins")
         country_data = covid2.get_status_by_country_name(country)
         if country_data:
             output_text += (
-                "`Last update : "
+                "`📅Last update : "
                 f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%Y-%m-%d %H:%M:%S')}`\n"
             )
             output_text += f"`Date provided by `[Johns Hopkins University](https://j.mp/2xf6oxF)"
