c5f8cfda09ee4f1d349488d97fe33eafc3962bab
HafizZiq <hafizuddinismail552@gmail.com>
2020-06-18 17:59:06 +0800

    typewriter: meh
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- userbot/modules/typewriter.py
+++ userbot/modules/typewriter.py
@@ -10,10 +10,9 @@ from userbot import CMD_HELP
 async def _(event):
     if event.fwd_from:
         return
-    # https://t.me/DirtyBlack_EXTENDED
     input_str = event.pattern_match.group(1)
     typing_symbol = "|"
-    DELAY_BETWEEN_EDITS = 0.3
+    DELAY_BETWEEN_EDITS = 0.1
     previous_text = ""
     await event.edit(typing_symbol)
     await asyncio.sleep(DELAY_BETWEEN_EDITS)
