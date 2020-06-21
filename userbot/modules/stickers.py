dea00da251c56a5fd4a2382a5fd6d29f6263f015
HafizZiq <hafizuddinismail552@gmail.com>
2020-06-22 02:25:50 +0800

    stickers: abader gayass
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -350,7 +350,7 @@ CMD_HELP.update({
 \n\n.kang [emoji('s)]\
 \nUsage: Works just like .kang but uses the emoji('s) you picked.\
 \n\n.kang [number]\
-\nUsage: Kang's the sticker/image to the specified pack but uses ðŸ¤” as emoji.\
+\nUsage: Kang's the sticker/image to the specified pack but uses 🤔 as emoji.\
 \n\n.kang [emoji('s)] [number]\
 \nUsage: Kang's the sticker/image to the specified pack and uses the emoji('s) you picked.\
 \n\n.stinfo\
