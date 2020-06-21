8b01a2faadc4a2912e0dc270247ccfd4b771b7b4
HafizZiq <hafizuddinismail552@gmail.com>
2020-06-22 02:49:40 +0800

    itv: add new plugin
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

---
+++ userbot/modules/itv.py
@@ -0,0 +1,39 @@
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot import CMD_HELP
+from userbot.events import register
+
+@register(outgoing=True, pattern=r"^.itv(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to a Link.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit("```Reply to a Link```")
+       return
+    chat = "@chotamreaderbot"
+    sender = reply_message.sender
+    await event.edit("```Processing```")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=272572121))
+              await event.client.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("`RIP Check Your Blacklist Boss`")
+              return
+          if response.text.startswith(""):
+             await event.edit("Am I Dumb Or Am I Dumb?")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
+
+CMD_HELP.update({
+    'itv':
+    '.itv <reply to link>\
+        \nUsage: Make instant view from any article.'
+})
