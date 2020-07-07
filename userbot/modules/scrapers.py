d9c61f7469948969c80e14eb93aa9d2fe216df76
HafizZiq <hafizuddinismail552@gmail.com>
2020-07-07 20:38:54 +0800

    scrapers: ;_;
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -719,11 +719,18 @@ async def WooMai(netase):
     chat = "@WooMaiBot"
     link = f"/netease {song}"
     await netase.edit("```Getting Your Music```")
+    sed = "未找到结果"
     async with bot.conversation(chat) as conv:
           await netase.edit("`Downloading...Please wait`")
           try:
               msg = await conv.send_message(link)
               response = await conv.get_response()
+              if sed in response:
+                  await netase.edit("`no results found`")
+                  await netase.client.delete_messages(conv.chat_id,
+                                                     [msg.id, response.id])
+                  await netase.delete()
+                  return
               respond = await conv.get_response()
               """ - don't spam notif - """
               await bot.send_read_acknowledge(conv.chat_id)
@@ -744,6 +751,7 @@ async def DeezLoader(Deezlod):
     d_link = Deezlod.pattern_match.group(1)
     if ".com" not in d_link:
         await Deezlod.edit("` I need a link to download something pro.`**(._.)**")
+        return
     else:
         await Deezlod.edit("**Initiating Download!**")
     chat = "@DeezLoadBot"
@@ -774,15 +782,23 @@ async def SpoMusDown(TifyDown):
     link = TifyDown.pattern_match.group(1)
     if ".com" in link:
         await TifyDown.edit("```I need Artist - Song Title, not your shitty link ;_;```")
+        return
     else:
         await TifyDown.edit("```Getting Your Music```")
     chat = "@SpotifyMusicDownloaderBot"
+    sed = "Couldn't find that:("
     async with bot.conversation(chat) as conv:
           await TifyDown.edit("`Downloading music taking some times,  Stay Tuned.....`")
           try:
               msg = await conv.send_message(link)
               song = await conv.get_response()
               pubdata = await conv.get_response()
+              if sed in pubdata:
+                  await TifyDown.edit(f"`{sed}`")
+                  await TifyDown.client.delete_messages(conv.chat_id,
+                                                     [msg.id, song.id, pubdata.id])
+                  await TifyDown.delete()
+                  return
               """ - don't spam notif - """
               await bot.send_read_acknowledge(conv.chat_id)
           except YouBlockedUserError:
