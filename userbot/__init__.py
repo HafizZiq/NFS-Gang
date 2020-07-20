5d8c9b7fc58195fc40a4f1ad4945e1359c3263d6
HafizZiq <hafizuddinismail552@gmail.com>
2020-07-21 00:31:25 +0800

    init: alive_logo: set default logo/image alive
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -136,7 +136,7 @@ YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY") or None
 ALIVE_NAME = os.environ.get("ALIVE_NAME") or None
 
 # Default .alive logo
-ALIVE_LOGO = os.environ.get("ALIVE_LOGO") or None
+ALIVE_LOGO = os.environ.get("ALIVE_LOGO") or "https://telegra.ph/file/c269a63ed3b26f82f4f7b.jpg"
 
 # Time & Date - Country and Time Zone
 COUNTRY = str(os.environ.get("COUNTRY") or "")
