d83340228ac0792427ed15e76212f902eebf7644
HafizZiq <hafizuddinismail552@gmail.com>
2020-10-21 21:31:37 +0800

    afk: revert pylint
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -11,16 +11,7 @@ from random import choice, randint
 
 from telethon.events import StopPropagation
 
-from userbot import (
-    AFKREASON,
-    COUNT_MSG,
-    CMD_HELP,
-    ISAFK,
-    BOTLOG,
-    BOTLOG_CHATID,
-    USERS,
-    PM_AUTO_BAN)  # pylint: disable=unused-imports
-
+from userbot import AFKREASON, BOTLOG, BOTLOG_CHATID, CMD_HELP, COUNT_MSG, ISAFK, USERS, PM_AUTO_BAN # noqa
 from userbot.events import register
 
 # ========================= CONSTANTS ============================
