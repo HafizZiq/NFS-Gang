7729ef38984c50035cf52807064822c0d4a9f03d
HafizZiq <hafizuddinismail552@gmail.com>
2020-10-21 21:43:40 +0800

    covid: add dot separator
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -17,12 +17,12 @@ async def corona(event):
     try:
         country_data = covid.get_status_by_country_name(country)
         output_text = (
-            f"`😷Confirmed   : {country_data['confirmed']} (+{country_data['new_cases']})`\n" +
-            f"`🤒Active      : {country_data['active']}`\n" +
-            f"`🤕Critical    : {country_data['critical']}`\n" +
-            f"`⚰Deaths      : {country_data['deaths']} (+{country_data['new_deaths']})`\n" +
-            f"`😇Recovered   : {country_data['recovered']}`\n" +
-            f"`🧪Total tests : {country_data['total_tests']}`\n" +
+            f"`😷Confirmed   : {format_integer(country_data['confirmed'])} (+{format_integer(country_data['new_cases'])})`\n" +
+            f"`🤒Active      : {format_integer(country_data['active'])}`\n" +
+            f"`🤕Critical    : {format_integer(country_data['critical'])}`\n" +
+            f"`⚰Deaths      : {format_integer(country_data['deaths'])} (+{format_integer(country_data['new_deaths'])})`\n" +
+            f"`😇Recovered   : {format_integer(country_data['recovered'])}`\n" +
+            f"`🧪Total tests : {format_integer(country_data['total_tests'])}`\n" +
             f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})")
         await event.edit(f"`Corona Virus Info in {country}:`\n\n{output_text}")
     except ValueError:
@@ -39,11 +39,11 @@ async def corona(event):
     try:
         country_data = covid.get_status_by_country_name(country)
         output_text = (
-            f"`😷Confirmed   : {country_data['confirmed']} (+{country_data['new_cases']})`\n" +
-            f"`🤒Active      : {country_data['active']}`\n" +
-            f"`🤕Critical    : {country_data['critical']}`\n" +
-            f"`⚰Deaths      : {country_data['deaths']} (+{country_data['new_deaths']})`\n" +
-            f"`😇Recovered   : {country_data['recovered']}`\n" +
+            f"`😷Confirmed   : {format_integer(country_data['confirmed'])} (+{format_integer(country_data['new_cases'])})`\n" +
+            f"`🤒Active      : {format_integer(country_data['active'])}`\n" +
+            f"`🤕Critical    : {format_integer(country_data['critical'])}`\n" +
+            f"`⚰Deaths      : {format_integer(country_data['deaths'])} (+{format_integer(country_data['new_deaths'])})`\n" +
+            f"`😇Recovered   : {format_integer(country_data['recovered'])}`\n" +
             f"`🧪Total tests : N/A`\n" +
             f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})")
         await event.edit(f"`Corona Virus Info in {country}:`\n\n{output_text}")
@@ -61,18 +61,37 @@ async def corona(event):
     try:
         country_data = covid.get_status_by_country_name(country)
         output_text = (
-            f"`😷Confirmed   : {country_data['confirmed']} (+{country_data['new_cases']})`\n" +
-            f"`🤒Active      : {country_data['active']}`\n" +
-            f"`🤕Critical    : {country_data['critical']}`\n" +
-            f"`⚰Deaths      : {country_data['deaths']} (+{country_data['new_deaths']})`\n" +
-            f"`😇Recovered   : {country_data['recovered']}`\n" +
-            f"`🧪Total tests : {country_data['total_tests']}`\n" +
+            f"`😷Confirmed   : {format_integer(country_data['confirmed'])} (+{format_integer(country_data['new_cases'])})`\n" +
+            f"`🤒Active      : {format_integer(country_data['active'])}`\n" +
+            f"`🤕Critical    : {format_integer(country_data['critical'])}`\n" +
+            f"`⚰Deaths      : {format_integer(country_data['deaths'])} (+{format_integer(country_data['new_deaths'])})`\n" +
+            f"`😇Recovered   : {format_integer(country_data['recovered'])}`\n" +
+            f"`🧪Total tests : {format_integer(country_data['total_tests'])}`\n" +
             f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})")
         await event.edit(f"`Corona Virus Info in {country}:`\n\n{output_text}")
     except ValueError:
         await event.edit(
             f"No information found for: {country}!\nCheck your spelling and try again."
         )
+
+def format_integer(number, thousand_separator="."):
+    def reverse(string):
+        string = "".join(reversed(string))
+        return string
+
+    s = reverse(str(number))
+    count = 0
+    result = ""
+    for char in s:
+        count = count + 1
+        if count % 3 == 0:
+            if len(s) == count:
+                result = char + result
+            else:
+                result = thousand_separator + char + result
+        else:
+            result = char + result
+    return result
 
 CMD_HELP.update({"covid": "`.covid `**<country>**"
                  "\n`Usage: Get an information about covid-19 data in your country.`\n\n"
